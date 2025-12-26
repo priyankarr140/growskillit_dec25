fc0f327524c300c55a99e60fdfdb5759958a9444
priyanka <priyankarr140@gmail.com>
2025-12-26 22:24:44 +0530

    assignment 26-12-2025

---
+++ Program5_Reverse.java
@@ -0,0 +1,13 @@
+package day3;
+public class Program5_Reverse {
+	public static void main(String[] args) {
+		int num=10;
+		while(num>=0)
+		{
+			System.out.println(num);
+			num--;
+		}
+
+	}
+
+}
